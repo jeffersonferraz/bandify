50dd790e842a7f653b36ee4f801749ad0c7d9c2d
Jefferson Ferraz <jefferson.ferraz@live.com>
2024-09-11 13:15:46 +0200

    Added objects to .gitignore and created /public directory

---
+++ src/public/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bandify</title>
+</head>
+<body>
+    <h1>Bandify</h1>
+</body>
+</html>
