13b05b4b9ceac694827bd49594de5da32bc5d05e
Jefferson Ferraz <jefferson.ferraz@live.com>
2024-09-11 17:18:08 +0200

    Update index.php

--- src/public/index.php
+++ src/public/index.php
@@ -3,9 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>bandify</title>
+    <title>bandify | Let's make some noise together!</title>
 </head>
 <body>
     <h1>bandify</h1>
+    <p>Let's make some noise together!</p>
 </body>
 </html>
