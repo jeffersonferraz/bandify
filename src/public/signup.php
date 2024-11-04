2afcb58b4049c29e017761328855089717213b5b
Jefferson Ferraz <jefferson.ferraz@live.com>
2024-11-04 09:49:35 +0100

    update main

--- src/public/signup.php
+++ src/public/signup.php
@@ -21,29 +21,6 @@
            <button name="signup-submit" type="submit">sign up</button><br>
            <a href="login.php" class="button">log in</a>
         </form>
-        <?php
-
-        require '../classes/User.class.php';
-
-        if (isset($_POST['signup_button'])) {
-            $user = new User();
-            $data = [
-                'firstname' => $_POST['first_name'],
-                'lastname' => $_POST['last_name'],
-                'email' => $_POST['email'],
-                'password' => $_POST['password']
-            ];
-
-            if ($user->create($data)) {
-
-                echo "User created successfully!";
-
-            } else {
-
-                echo "Please fill in all required fields.";
-            }
-        }
-        ?>
     </div>
 </body>
 </html>
