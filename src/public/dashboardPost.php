6287efc7f4bfdfcafe9bfa2764a2e6e61b7140fb
nathanplenty <nathan.roskamp@plentysystems.com>
2024-11-05 09:57:45 +0100

    Update dashboardPost.php

--- src/public/dashboardPost.php
+++ src/public/dashboardPost.php
@@ -18,6 +18,44 @@
         <a href="updatePost.php"><button class="crud-button">Update</button></a>
         <a href="deletePost.php"><button class="crud-button">Delete</button></a>
     </div>
+
+    <?php
+    if (isset($_GET['action'])) {
+        switch ($_GET['action']) {
+            case 'create':
+                ?>
+                <h2>Create Post</h2>
+                <form action="../includes/createPost.inc.php" method="post">
+                    <input class="input-data" name="title" type="text" placeholder=" Title" required><br>
+                    <textarea class="input-data" name="description" placeholder=" Description"></textarea><br>
+                    <button class="submit-button" name="post-submit" type="submit">create</button><br>
+                </form>
+                <?php
+                break;
+
+            case 'read':
+                ?>
+                <h2>Posts</h2>
+                <?php require '../includes/readPost.inc.php'; ?>
+                <?php
+                break;
+
+            // Hier kannst du weitere Fälle für 'update' und 'delete' hinzufügen
+            case 'update':
+                echo "<h2>Update Post</h2>";
+                // Füge hier die Logik für das Aktualisieren von Posts hinzu
+                break;
+
+            case 'delete':
+                echo "<h2>Delete Post</h2>";
+                // Füge hier die Logik für das Löschen von Posts hinzu
+                break;
+
+            default:
+                echo "<p>Bitte wähle eine Aktion aus.</p>";
+        }
+    }
+    ?>
 </div>
 </body>
 </html>
