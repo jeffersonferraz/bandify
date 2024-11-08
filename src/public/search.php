f2ea99fdc4146119535c9621db415fc1b721817f
Jefferson Ferraz <jefferson.ferraz@live.com>
2024-11-08 09:14:59 +0100

    changes on search page structure

--- src/public/search.php
+++ src/public/search.php
@@ -9,6 +9,7 @@
 </head>
 <body>
 <?php
+// header should match the logged and non logged user
 include("../includes/header.inc.php");
 ?>
 <div class="container">
@@ -26,21 +27,27 @@ include("../includes/header.inc.php");
     <div id="post" class="category">
         <form action="../includes/search.inc.php" method="post">
             <input class="input-data" name="post" type="text" placeholder=" post name"><br>
-            <button class="submit-button" name="signup-submit" type="submit">search</button><br>
+            <input class="input-data" name="city" type="text" placeholder=" city"><br>
+            <p>Manual or automatic matching function?</p>
+            <button class="submit-button" name="search-post-submit" type="submit">search</button><br>
+            <a href="#" class="button">manual</a>
+            <a href="#" class="button">automatic</a>
         </form>
     </div>
 
     <div id="musician" class="category" style="display:none">
         <form action="../includes/search.inc.php" method="post">
             <input class="input-data" name="musician" type="text" placeholder=" musician"><br>
-            <button class="submit-button" name="signup-submit" type="submit">search</button><br>
+            <input class="input-data" name="city" type="text" placeholder=" city"><br>
+            <button class="submit-button" name="search-musician-submit" type="submit">search</button><br>
         </form>
     </div>
 
     <div id="music-group" class="category" style="display:none">
         <form action="../includes/search.inc.php" method="post">
             <input class="input-data" name="music-group" type="text" placeholder=" music group"><br>
-            <button class="submit-button" name="signup-submit" type="submit">search</button><br>
+            <input class="input-data" name="city" type="text" placeholder=" city"><br>
+            <button class="submit-button" name="signup-music-group-submit" type="submit">search</button><br>
         </form>
     </div>
 
